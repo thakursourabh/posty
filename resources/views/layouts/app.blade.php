e1319a5c220fc4a1e66781d85c706050b01ea7e6
thakursourabh <sourabhpatel@live.in>
2022-03-07 17:54:10 +0530

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Posty</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body class="bg-gray-200">
+    <nav class="p-6 bg-white flex justify-between mb-6">
+        <ul class="flex items-center">
+            <li>
+                <a href="http://" class="p-3">Home</a>
+            </li>
+            <li>
+                <a href="http://" class="p-3">Dashboard</a>
+            </li>
+            <li>
+                <a href="http://" class="p-3">Posts</a>
+            </li>
+        </ul>
+
+        <ul class="flex items-center">
+            <li>
+                <a href="http://" class="p-3">Sourabh </a>
+            </li>
+            <li>
+                <a href="http://" class="p-3">Login</a>
+            </li>
+            <li>
+                <a href="{{ route('register') }}" class="p-3">Register</a>
+            </li>
+        </ul>
+    </nav>
+    @yield('content')
+</body>
+</html>
