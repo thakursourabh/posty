080e98fdf64671aef7b6e242af56312c753792d0
thakursourabh <sourabhpatel@live.in>
2022-03-07 22:26:14 +0530

    auth functionality

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,10 +11,10 @@
     <nav class="p-6 bg-white flex justify-between mb-6">
         <ul class="flex items-center">
             <li>
-                <a href="http://" class="p-3">Home</a>
+                <a href="/" class="p-3">Home</a>
             </li>
             <li>
-                <a href="http://" class="p-3">Dashboard</a>
+                <a href="{{ route('dashboard') }}" class="p-3">Dashboard</a>
             </li>
             <li>
                 <a href="http://" class="p-3">Posts</a>
@@ -22,15 +22,27 @@
         </ul>
 
         <ul class="flex items-center">
-            <li>
-                <a href="http://" class="p-3">Sourabh </a>
-            </li>
-            <li>
-                <a href="http://" class="p-3">Login</a>
-            </li>
-            <li>
-                <a href="{{ route('register') }}" class="p-3">Register</a>
-            </li>
+            @auth
+                <li>
+                    <a href="http://" class="p-3">{{ auth()->user()->name }}</a>
+                </li>
+                <li>
+                    <form action="{{ route('logout') }}" method="post" class="p-3 inline">
+                        @csrf
+                        <button type="submit">Logout</button>
+                    </form>
+                </li>
+            @endauth
+
+            @guest
+                <li>
+                    <a href="{{ route('login') }}" class="p-3">Login</a>
+                </li>
+                <li>
+                    <a href="{{ route('register') }}" class="p-3">Register</a>
+                </li>
+            @endguest
+
         </ul>
     </nav>
     @yield('content')
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 
 /*
@@ -13,9 +16,22 @@ use App\Http\Controllers\Auth\RegisterController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+   return view('home');
+})->name('home');
 
 Route::get('/register',[RegisterController::class, 'index'])->name('register');
+Route::post('/register',[RegisterController::class, 'store']);
+
+Route::get('/login',[LoginController::class, 'index'])->name('login');
+Route::post('/login',[LoginController::class, 'store']);
+
+Route::post('/logout',[LogoutController::class, 'store'])->name('logout');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::get('/posts', function () {
     return view('posts.index');
 });
+
+
